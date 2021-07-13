12be2369ec3ddc78086d7cda13989d055a712ee8
Rubí <86703266+RubiSaCa@users.noreply.github.com>
2021-07-12 19:58:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    function (){
+
+	document.getElementById('formulario').reset();
+
+}
+
+ 
+
+function multiplicar(){
+
+	var x = parsefloat(document.getElementById('valor1').value;
+
+	var y = parsefloat(document.getElementById('valor2').value;
+
+	var z = parsefloat(document.getElementById('valor3').value;
+
+	document.getElementById('el-resultado').innerHTML = x*y*z;
+
+}
     
 }
-        
+
